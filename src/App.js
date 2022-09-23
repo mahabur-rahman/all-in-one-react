add9062668d88af81609248330216ba041a19123
mahabur  rahman <annur4395@gmail.com>
2022-09-23 19:56:50 +0600

    finish custom validate form with error handling

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ const App = () => {
       errorMessage:
         "Username should be 3-16 characters and shouldn't include any special character!",
       label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
       required: true,
     },
     {
@@ -67,6 +68,7 @@ const App = () => {
       errorMessage:
         "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
       label: "Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
       required: true,
     },
     {
@@ -76,6 +78,7 @@ const App = () => {
       placeholder: "Confirm Password",
       errorMessage: "Passwords don't match!",
       label: "Confirm Password",
+      pattern: values.password,
       required: true,
     },
   ];
@@ -93,6 +96,7 @@ const App = () => {
       <div className="app">
         <form onSubmit={handleSubmit}>
           <h1>Register</h1>
+          <br />
           {inputs.map((input) => (
             <FormInput
               key={input.id}
--- src/components/FormValidaton/FormTwo.js
+++ src/components/FormValidaton/FormTwo.js
@@ -4,10 +4,24 @@ import "./form.css";
 const FormInput = (props) => {
   const { label, errorMessage, onChange, id, ...inputProps } = props;
 
+  const [focused, setFocused] = useState(false);
+
+  const handleFocus = () => {
+    setFocused(true);
+  };
+
   return (
     <div className="formInput">
       <label>{label}</label>
-      <input {...inputProps} onChange={onChange} />
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
       <span>{errorMessage}</span>
     </div>
   );
